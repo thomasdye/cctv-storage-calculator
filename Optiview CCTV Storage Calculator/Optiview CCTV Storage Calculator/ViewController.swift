a2d5fbe9f89205e5ee588b7a49f8772d5cc35831
ThomasDye <thomasallendye@gmail.com>
2019-10-01 13:35:04 -0400

    added motion detect functionality

--- Optiview CCTV Storage Calculator/Optiview CCTV Storage Calculator/ViewController.swift
+++ Optiview CCTV Storage Calculator/Optiview CCTV Storage Calculator/ViewController.swift
@@ -11,19 +11,22 @@ import Foundation
 
 class ViewController: UIViewController {
     
-
-    @IBOutlet weak var storageTotalLabel: UILabel!
+    @IBOutlet weak var totalStorageLabel: UILabel!
     @IBOutlet weak var totalCamerasTextField: UITextField!
     @IBOutlet weak var bitrateTextField: UITextField!
-    @IBOutlet weak var numberOfDaysTextField: UITextField!
-    @IBOutlet weak var recordingHoursTextField: UITextField!
+    @IBOutlet weak var totalDaysTextField: UITextField!
+    @IBOutlet weak var totalHoursTextField: UITextField!
     @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var totalCamerasStepper: UIStepper!
+    @IBOutlet weak var motionDetectSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var totalDaysStepper: UIStepper!
+    @IBOutlet weak var totalHoursStepper: UIStepper!
     
-    var totalCams: Int = 0
+    var totalCameras: Int = 0
     var resolution: Int = 2
     var camBitrate: Int = 1024
     var totalDays: Int = 0
-    var recordingHours: Int = 0
+    var totalHours: Int = 0
     let convertSecondsToHour: Int = 3600
     var motionDetectOn: Bool = true
     var isGigabyte: Bool = true
@@ -33,26 +36,72 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setup()
+
     }
 
     @IBAction func calculateButtonTapped(_ sender: UIButton) {
         
         camBitrate = Int(bitrateTextField.text!)!
-        recordingHours = Int(recordingHoursTextField.text!)!
-        totalCams = Int(totalCamerasTextField.text!)!
-        totalDays = Int(numberOfDaysTextField.text!)!
+        totalHours = Int(totalHoursTextField.text!)!
+        totalCameras = Int(totalCamerasTextField.text!)!
+        totalDays = Int(totalDaysTextField.text!)!
         
-        storageGB = ((camBitrate / 8) * convertSecondsToHour * recordingHours * totalCams * totalDays)
+        storageGB = ((camBitrate / 8) * convertSecondsToHour * totalHours * totalCameras * totalDays)
         
         storageGB = storageGB / 1_000_000
+        
+        if motionDetectOn == true {
+            storageGB = storageGB / 2
+        }
         
         if storageGB >= 1000 {
           storageTB = storageGB / 1000
             
-          storageTotalLabel.text = "\(storageTB) TB"
+          totalStorageLabel.text = "\(storageTB) TB"
         } else {
-            storageTotalLabel.text = "\(storageGB) GB"
+            totalStorageLabel.text = "\(storageGB) GB"
         }
+    }
+    
+    @IBAction func totalCamerasStepperPressed(_ sender: UIStepper) {
+        
+        totalCamerasTextField.text = Int(sender.value).description
+    }
+    
+    @IBAction func totalDaysStepperPressed(_ sender: UIStepper) {
+        
+        totalDaysTextField.text = Int(sender.value).description
+    }
+    
+    @IBAction func totalHoursStepperPressed(_ sender: UIStepper) {
+        
+        totalHoursTextField.text = Int(sender.value).description
+    }
+    
+    @IBAction func motionDetectSegementedControlPressed(_ sender: UISegmentedControl) {
+        
+        switch motionDetectSegmentedControl.selectedSegmentIndex
+         {
+         case 0:
+             motionDetectOn = false
+         case 1:
+             motionDetectOn = true
+         default:
+             break
+         }
+    }
+    
+    func setup() {
+        totalCamerasTextField.text = "1"
+        totalDaysTextField.text = "30"
+        totalHoursTextField.text = "24"
+        totalCamerasStepper.value = 1
+        totalDaysStepper.value = 30
+        totalHoursStepper.value = 24
+        totalHoursStepper.maximumValue = 24
+        totalHoursStepper.minimumValue = 1
+        totalStorageLabel.text = ""
     }
 }
 
