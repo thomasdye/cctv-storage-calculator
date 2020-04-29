a76c49b87c04291d64caccabbb62042e49436abf
Thomas Dye <thomasallendye@gmail.com>
2020-04-28 20:44:18 -0400

    added job struct

---
+++ CCTV Storage Calculator/Optiview CCTV Storage Calculator/Model/Job.swift
@@ -0,0 +1,20 @@
+//
+//  Job.swift
+//  CCTV Storage Calculator
+//
+//  Created by Thomas Dye on 4/28/20.
+//  Copyright © 2020 Thomas Dye. All rights reserved.
+//
+
+import Foundation
+
+struct Job {
+    var jobName: String
+    var customerName: String
+    var phoneNumber: String
+    var systemType: Int
+    var numberOfCameras: Int
+    var totalStorage: String
+    var notes: String?
+
+}
